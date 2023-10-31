99e69930bc53ab7c1604443fd391250da8fffc95
bonface murangiri <bonnyrangi95@gmail.com>
2023-10-31 12:20:17 +0000

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,9 +12,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
 
-	if (!parent)
-		return (NULL);
-
 	newnode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 	if (!newnode)
 		return (NULL);
